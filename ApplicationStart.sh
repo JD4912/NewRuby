56f9236e1f6da1c17788594b9af0fc8d027c2a69
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-05-11 19:06:16 +0530

    Update ApplicationStart.sh

--- ApplicationStart.sh
+++ ApplicationStart.sh
@@ -2,15 +2,15 @@
 # sudo su -source /usr/bin/ruby2.7 
 source /usr/bin/ruby2.7 
 cd /Newruby
-rbenv install 2.7.2
-rbenv use 2.7.2
+# rbenv install 2.7.2
+# rbenv use 2.7.2
 gem install bundle
 bundle install
-pwd
-ls -ltrh
-whoami
-sudo su -
-ruby -v
-gem install rails -v 6.1.5
-cd /Newruby
+# pwd
+# ls -ltrh
+# whoami
+# sudo su -
+# ruby -v
+# gem install rails -v 6.1.5
+# cd /Newruby
 rails server -b 0.0.0.0
